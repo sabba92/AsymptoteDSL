f52351881c2b7ebde69e54599eb3e18ab8f74957
sabba92 <sabba_92_@hotmail.it>
2020-05-03 22:35:15 +0200

    Updated file

--- src/main/scala/AsymptoteDSL.scala
+++ src/main/scala/AsymptoteDSL.scala
@@ -1,8 +1,8 @@
 import java.io._
 
 import atoms._
-import complex.{AsymptoteImage, PetriAuto, PetriGrid}
-import traits.AsymptoteObject
+import complex._
+import traits._
 
 import scala.language.postfixOps
 import sys.process._
@@ -11,31 +11,12 @@ object AsymptoteDSL extends App {
   val filename: String = "out.asy"
   val writer = new PrintWriter(new File(filename))
 
-  val petriA = new PetriAuto(List("P0", "P1", "P2"),
-    List("T1"),
-    Map("T1" -> List("P0")),
-    Map("T1" -> List("P1", "P2")),
-    List(("P0", 1)),
-    List(("P0", "T1", 5)),
-    List(("P1", "T1", 2)))
-
-  val petriG = new PetriGrid(Map("P0" -> (0, 4), "P1" -> (4, 4), "P2" -> (2, 0)),
-    Map("T1" -> (2, 4)),
-    Map("T1" -> List("P0")),
-    Map("T1" -> List("P1", "P2")),
-    List(("P0", 1)),
-    List(("P0", "T1", 5)),
-    List(("P1", "T1", 2)))
-
   val asyObj =
     new AsymptoteImage withProp List[AsymptoteObject](
-      new Prop("size(15cm);"),
-      new Prop("settings.outformat = \"pdf\";"),
-      //new Prop("size(21cm, 27cm);"), per petri
-      //new Prop("size(60cm, 30cm);"),
+      // put here properties
     ) having List[AsymptoteObject](
-      petriG
-    ) logoOn (0.5, -1)
+      // put here Asymptote objects
+    ) logoOn (0, 0)
 
   writer.write(asyObj toAsyString)
   writer.close()
